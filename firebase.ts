201ed5bac611461ea2570c07f2fac318e53e70ed
Sebastian <sebastianchavarry2001@gmail.com>
2024-08-11 11:42:57 -0500

    probando conexión a firebase funcionando

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDsFP_rzyrcf3ujlUJg03RNx8-eIIWGm5k",
+  authDomain: "pantry-app-demo.firebaseapp.com",
+  projectId: "pantry-app-demo",
+  storageBucket: "pantry-app-demo.appspot.com",
+  messagingSenderId: "866612730332",
+  appId: "1:866612730332:web:0a09c0778fc04c20b4d719",
+  measurementId: "G-3SH5M4C382",
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
+
+export const firestore = getFirestore(app);
